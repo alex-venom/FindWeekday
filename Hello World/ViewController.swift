4b6f9a9db1e31224370a37ff7499e3b34b829fad
Александр Нарывончик <96579815+alex-venom@users.noreply.github.com>
2022-02-13 15:35:31 +0300

    Add files via upload

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Aliaksandr Naryvonchyk on 25.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else {return}
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else {return}
+        
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekay = weekday.capitalized
+        
+        resultLabel.text = capitalizedWeekay
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
